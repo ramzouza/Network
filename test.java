95b9dc0ff4ddefc6553b884fe22889a8e359985c
Ramez Zaid <rzaid@genetec.com>
2020-02-01 16:30:32 -0500

    test file that demonstrates how to use socket

---
+++ test.java
@@ -0,0 +1,49 @@
+
+//Blocking Classes
+import java.net.Socket;
+import java.net.ServerSocket;
+
+//Non Blocking Classes
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+import java.nio.ByteBuffer;
+import java.io.PrintWriter;
+//Misc Classes
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.URL;
+import java.net.URI;
+
+class test {
+
+    public static void main(String[] args) {
+       
+        try {
+            InetAddress web = InetAddress.getByName("www.httpbin.org");
+             Socket sock = new Socket(web,80);
+             PrintWriter out = new PrintWriter(sock.getOutputStream());
+             Scanner in = new Scanner(sock.getInputStream());        
+     
+             out.write("GET /status/418 HTTP/1.0\r\nUser-Agent: Hello\r\n\r\n");
+             out.flush();
+     
+             while(in.hasNextLine()) {
+             System.out.println(in.nextLine());
+             }
+     
+             out.close();
+             in.close();
+             sock.close();
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+      
+      
+
+
+    }
+
+}
